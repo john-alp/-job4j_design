c72a5f988307ed629a61e25a1e3e08635a70ee45
demiurg <john-alp@mail.ru>
2021-09-24 10:23:51 +0300

    2.1.1. Iterator (fixed)

--- src/main/java/ru/job4j/BackwardArrayIt.java
+++ src/main/java/ru/job4j/BackwardArrayIt.java
@@ -6,15 +6,16 @@ import java.util.NoSuchElementException;
 public class BackwardArrayIt implements Iterator<Integer> {
 
     private final int[] data;
-    private int point = 0;
+    private int point;
 
     public BackwardArrayIt(int[] data) {
         this.data = data;
+        this.point = data.length - 1;
     }
 
     @Override
     public boolean hasNext() {
-        return point < data.length;
+        return point >= 0;
     }
 
     @Override
@@ -22,12 +23,6 @@ public class BackwardArrayIt implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        int[] result = new int[data.length];
-        for (int i = data.length - 1, j = 0; i >= 0; i--, j++) {
-            result[j] = data[i];
-        }
-        return result[point++];
+        return data[point--];
     }
 }
-
-
