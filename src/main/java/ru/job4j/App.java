6b130b4a65097303195dea076d78c762abecbf39
demiurg <john-alp@mail.ru>
2021-09-27 18:46:24 +0300

    5.1.1. Iterator for [][] (fix)

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -11,7 +11,7 @@ class App {
         int column = 0;
         int row = 0;
 
-        int data[][] = {
+        int[][] data = {
                 {1, 2, 3, 4},
                 {6, 7, 8, 9},
                 {10, 11, 12, 13}
