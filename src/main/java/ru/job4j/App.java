44287cee255e3fff54169a50835104d59ed981da
demiurg <john-alp@mail.ru>
2021-09-27 18:43:18 +0300

    5.1.1. Iterator for [][]

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,10 +1,39 @@
-//package ru.job4j;
+package ru.job4j;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Iterator;
+
 //
-///**
-// * Hello world!
-// */
-//public class App {
-//    public static void main(String[] args) {
-//        System.out.println("Hello World!");
-//    }
 //}
+class App {
+    public static void main(String[] args) {
+        int column = 0;
+        int row = 0;
+
+        int data[][] = {
+                {1, 2, 3, 4},
+                {6, 7, 8, 9},
+                {10, 11, 12, 13}
+        };
+
+
+//        for (; row <= data.length - 1; row++) {
+//            for (; column <= data[row].length - 1; column++) {
+//                System.out.println(data[row][column]);
+//            }
+//            column = 0;
+//            row++;
+//        }
+        while (row <= data.length - 1) {
+            while (column <= data[row].length - 1) {
+                System.out.println(data[row][column]);
+            column++;
+            }
+        column = 0;
+        row++;
+        }
+        System.out.println(column + " " + row);
+    }
+
+}
