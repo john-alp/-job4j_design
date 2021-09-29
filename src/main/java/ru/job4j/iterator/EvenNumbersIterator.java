f482d41763599bf3326f8ad9da3593f697fddaae
demiurg <john-alp@mail.ru>
2021-09-29 10:01:26 +0300

    5.1.2. Iterator. Возвращает четные числа (fix3)

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -4,31 +4,32 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- * 28.09.2021
  * Создать итератор возвращающий только четные цифры.
  *
  * Итератор должен принимать список произвольных чисел.
  * Метод next должен возвращать верные значения вне зависимости от того вызвал ли перед этим программист метод hasNext. Аналогично для hasNext.
  * Результат работы ваших методов не должен зависеть от последовательности в которой программист вызывает методы.
- */
+ *
+ * @author ESolomatin
+ * @version 1.1
+ * @since 28.09.2021
+ *
+ **/
 
 public class EvenNumbersIterator implements Iterator<Integer> {
     private final int[] data;
-    private final int size;
-    private int index = -1;
-    private int evenIndex;
+    private int index;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
-        this.size = data.length;
     }
 
     @Override
     public boolean hasNext() {
         boolean result = false;
-        for (int i = index + 1; i < size; i++) {
+        for (int i = index; i < data.length; i++) {
             if (data[i] % 2 == 0) {
-                evenIndex = i;
+                index = i;
                 result = true;
                 break;
             }
@@ -39,9 +40,9 @@ public class EvenNumbersIterator implements Iterator<Integer> {
     @Override
     public Integer next() throws NoSuchElementException {
         if (hasNext()) {
-            index = evenIndex;
-            return data[index];
+            return data[index++];
+        } else {
+            throw new NoSuchElementException("There is no next element");
         }
-        throw new NoSuchElementException();
     }
 }
